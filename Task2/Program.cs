7bbeff6b2d6fa10a15d95ca7cbdc6cd9d72b4e3a
Zarema <kzariema@mail.ru>
2023-05-03 23:11:14 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.WriteLine("Введите число: ");
+
+int x = Convert.ToBase64CharArray(Console.ReadLine());
+int result = x % 10;
+
+if (x / 100 > 0) 
+{
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
